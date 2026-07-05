8ce310539bcf86fde0da3328f27f9596e94efe75
hamipasha43-ctrl <hamipasha43@gmail.com>
2026-07-05 15:42:49 +0500

    complete push

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 const products = []
-
-fetch("https://api.escuelajs.co/api/v1/products")
+fetch("https://dummyjson.com/recipes")
     .then(response => response.json())
-    .then(data => products.push(...data))
+    .then(data => products.push(...data.recipes))
     .catch(error => console.error('Error:', error));
 
 console.log(products)
 
+
 let container = document.querySelector("#main")
 
+function showLoader(){
+  let loader = document.querySelector(".loader")
+  loader.style.display="block"
+}
+function hideLoader(){
+  let loader = document.querySelector(".loader")
+  loader.style.display="none"
+}
+showLoader()
+
 setTimeout(() => {
-    for (let i = 0; i < products.length; i++) {
-        console.log(products[i])
-        let product = products[i]
-        container.innerHTML += `<div class="card" style="width: 18rem;">
-        
-  <img src=${product?.images[0] || "images/burger.png"} class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${product.title}</h5>
-    <p class="card-text">${product.description}</p>
-    
-  </div>
+  hideLoader()
+  for (let i = 0; i < products.length; i++) {
+      console.log(products[i])
+      let product = products[i]
+      container.innerHTML += `<div class="card" style="width: 18rem;">
+      
+<img src=${product.image} class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">${product.name}</h5>
+  <p class="card-text">${product.instructions}</p>
+  
+</div>
 </div>`
 
-    }
+  }
 }, 2000)
